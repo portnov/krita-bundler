94a48f7e4a794c0612e8ba160664ce18b0b3cb14
Ilya Portnov <portnov@iportnov.ru>
2015-08-16 16:35:25 +0500

    Add clear-blacklist.py script.

---
+++ clear-blacklist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+from os.path import expanduser
+import argparse
+from lxml import etree
+
+def parse_cmdline():
+    parser = argparse.ArgumentParser(description='Remove blacklisted resources')
+    parser.add_argument('-f', '--force', action='store_true', help='Actually remove files. Without this flag - just list them.')
+    parser.add_argument('filename', metavar='FILENAME.blacklist', help='*.blacklist file')
+    return parser.parse_args()
+
+args = parse_cmdline()
+xml = etree.fromstring(open(args.filename).read())
+
+for e in xml.findall('file/name'):
+    path = expanduser(e.text)
+    if args.force:
+        os.remove(path)
+    else:
+        print("rm " + path)
+
